47521470fdcc9a9564e0a441ab1d79ac9873f87d
Codigo-AR <89418838+Codigo-AR@users.noreply.github.com>
2021-10-27 17:04:44 +0200

    Delete script.js

--- js/script.js
+++
@@ -1,4 +0,0 @@
-/*$('#copy').addEventListener('click', () => {
-    Portapapeles('<!-- JeS -->\n<script src="https://Codigo-AR.github.io/JeS/js/JeSpañol.js" src="anonymous"></script>')
-    alerta('Copiado al portapapeles')
-})*/
